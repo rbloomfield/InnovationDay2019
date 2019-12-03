816a3c52fdfe3ef62c9e4ce3db6fd0abbcfd68a3
Ollie T Bloomfield-Mcgarr <Ollie.BloomfieldMcgarr@vonage.com>
2019-12-03 09:19:33 +0000

    [wip] Call terminates at the end of the first NCCO
    need to find a way to keep the call open, could add timeout and loop the
    orinal NCCO

--- main.go
+++ main.go
@@ -12,27 +12,30 @@ const port string = ":8080"
 const eventURL string = "/"
 const answerURL string = "/answer"
 
+var call_id_to_recordings map[string][]string = make(map[string][]string)
+
 func main() {
 	router := gin.Default()
 
+	mainNCCO := []struct {
+		Text     string   `json:"text,omitempty"`
+		Action   string   `json:"action"`
+		EventURL []string `json:"eventUrl,omitempty"`
+		EndOnKey string   `json:"endOnKey,omitempty"`
+		Timeout  string   `json:"timeout,omitempty"`
+	}{
+		{
+			Action: "talk",
+			Text:   "Please say your passphrase",
+		},
+		{
+			Action:   "record",
+			EventURL: []string{"http://a9091a98.ngrok.io" + answerURL},
+			EndOnKey: "#",
+		},
+	}
 	router.GET(eventURL, func(c *gin.Context) {
-		temp := []struct {
-			Text     string   `json:"text,omitempty"`
-			Action   string   `json:"action"`
-			EventURL []string `json:"eventUrl,omitempty"`
-			EndOnKey string   `json:"endOnKey,omitempty"`
-		}{
-			{
-				Action: "talk",
-				Text:   "Please say your passphrase",
-			},
-			{
-				Action:   "record",
-				EventURL: []string{"http://a9091a98.ngrok.io" + answerURL},
-				EndOnKey: "#",
-			},
-		}
-		c.JSON(http.StatusOK, temp)
+		c.JSON(http.StatusOK, mainNCCO)
 	})
 
 	router.POST(answerURL, func(c *gin.Context) {
@@ -43,8 +46,23 @@ func main() {
 		if err := c.ShouldBindJSON(&json); err != nil {
 			c.JSON(http.StatusOK, nil)
 			return
+
 		}
+
 		fmt.Printf("%#v", json)
+		if i, ok := call_id_to_recordings[json.RecordingURL]; ok {
+			call_id_to_recordings[json.RecordingURL] = append(i, json.RecordingURL)
+			if len(i) >= 3 {
+				// here we should forward it on
+				c.JSON(http.StatusOK, nil)
+				return
+			}
+		}
+		call_id_to_recordings[json.RecordingURL] = append(make([]string, 3), json.RecordingURL)
+		fmt.Println("Replay NCCO")
+
+		c.JSON(http.StatusOK, mainNCCO)
+
 	})
 
 	router.Run(port)
